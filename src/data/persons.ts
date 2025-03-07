b2ad70d37089c1bcd162b5dfb8f572361d2deeeb
shakilkhan12 <info@majidkhanmohmand.com>
2025-03-07 16:05:57 +0500

    data updated

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -58,7 +58,7 @@ export const data: Person[] = [
         dues: 0,
         history: [
           { id: 1, name: 'شاہد والد', amount: 'وصول',deposit_amount: 1000 },
-          { id: 2, name: 'اصغر والدہ', amount: 1000,deposit_amount: 0 }
+          { id: 2, name: 'اصغر والدہ', amount:'وصول',deposit_amount: 1000  }
         ]
       }
     },
@@ -722,7 +722,7 @@ export const data: Person[] = [
         dues: 0,
         history: [
           { id: 1, name: 'شاہد والد', amount: 'وصول',deposit_amount: 1000 },
-          { id: 2, name: 'اصغر والدہ', amount: 1000,deposit_amount: 0 }
+          { id: 2, name: 'اصغر والدہ', amount:'وصول',deposit_amount: 1000  }
 
         ]
       }
@@ -735,7 +735,7 @@ export const data: Person[] = [
         dues: 0,
         history: [
           { id: 1, name: 'شاہد والد', amount: 'وصول',deposit_amount: 1000 },
-          { id: 2, name: 'اصغر والدہ', amount: 1000,deposit_amount: 0 }
+          { id: 2, name: 'اصغر والدہ', amount:'وصول',deposit_amount: 1000  }
 
         ]
       }
