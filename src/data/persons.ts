7aa818159c4d258798e01b1a9fd52fae768cb7de
shakilkhan12 <info@majidkhanmohmand.com>
2025-03-08 19:32:35 +0500

    data updated

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -277,9 +277,8 @@ export const data: Person[] = [
     },
     {
       id: 23, 
-      name: 'imran shamai',
-      urdu: 'عمران شمائ ',
-  
+      name: 'muhammad imran',
+      urdu: 'محمّد عمران ',
       details: {
         dues: 0,
         history: [
@@ -558,7 +557,7 @@ export const data: Person[] = [
         dues: 0,
         history: [
           { id: 1, name: 'شاہد والد', amount: 'وصول',deposit_amount: 1000 },
-          { id: 2, name: 'اصغر والدہ', amount: 1000,deposit_amount: 0 }
+          { id: 2, name: 'اصغر والدہ', amount:'وصول',deposit_amount: 1000   }
 
         ]
       }
