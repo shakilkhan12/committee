e4b5f9e52cd53ea0a8f422e8b0297b14f30b9a63
shakilkhan12 <info@majidkhanmohmand.com>
2025-04-21 21:08:28 +0500

    data updated

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -45,8 +45,8 @@ export const data: Person[] = [
       details: {
         dues: 4000,
         history: [
-          { id: 1, name: 'شاہد والد', amount: 1000, deposit_amount: 0 },
-          { id: 2, name: 'اصغر والدہ', amount: 1000,deposit_amount: 0 }
+          { id: 1, name: 'شاہد والد', amount: 'وصول',deposit_amount: 1000 },
+          { id: 2, name: 'اصغر والدہ', amount:'وصول',deposit_amount: 1000   }
         ]
       }
     },
