039e9931af16af758c1d73bb6e3c26774edaa787
shakilkhan12 <info@majidkhanmohmand.com>
2025-08-18 17:46:34 +0500

    data updated

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -904,11 +904,11 @@ export const data: Person[] = [
       urdu: 'پرویز ',
     
       details: {
-        dues: 3000,
+        dues: 0,
         history: [
           { id: 1, name: 'شاہد والد', amount: 'وصول',deposit_amount: 1000 },
           { id: 2, name: 'اصغر والدہ', amount:'وصول',deposit_amount: 1000, death_date: 'چھ مارچ   2025'   },
-          { id: 3, name: 'حاجی محمّد زادہ', amount:1000,deposit_amount: 0, death_date: ' چودہ  اگست  2025'}
+          { id: 3, name: 'حاجی محمّد زادہ', amount:'وصول',deposit_amount: 1000, death_date: ' چودہ  اگست  2025'}
 
         ]
       }
