c71858001cc089e011fc844efbb2ccab9055af5a
shakil khan <shakilkhan621@gmail.com>
2026-05-02 21:37:37 +0500

    data up

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -535,7 +535,7 @@ export const data: Person[] = [
           { id: 2, name: 'اصغر والدہ', amount:'وصول',deposit_amount: 1000,death_date: 'چھ مارچ   2025' },
           { id: 3, name: 'حاجی محمّد زادہ', amount:'وصول',deposit_amount: 1000, death_date: ' چودہ  اگست  2025'},
           { id: 4, name: 'شاہ تماز  مرحوم ', amount:'وصول',deposit_amount: 1000, death_date: 'بارہ دسمبر   2025'},
-           { id: 5, name: 'نیاز محمّد بہن ', amount:1000,deposit_amount: 0, death_date: 'پانچ اپریل  2026'},
+           { id: 5, name: 'نیاز محمّد بہن ', amount:'وصول',deposit_amount: 1000, death_date: 'پانچ اپریل  2026'},
         ]
       }
     },
