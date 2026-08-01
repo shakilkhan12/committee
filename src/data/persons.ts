6adbd884ea505fb155178dc67a9f72ba452cca91
shakil khan <shakilkhan621@gmail.com>
2026-08-01 09:23:17 +0500

    chnages makde

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -22,14 +22,14 @@ export const data: Person[] = [
     name: 'gull ghafar',
     urdu: 'گل  غفار ',
     details: {
-      dues: 450,
+      dues: '50',
       history: [
         { id: 1, name: 'شاہد والد', amount: 'وصول', deposit_amount: 1000 },
-        { id: 2, name: 'اصغر والدہ', amount: 1000, deposit_amount: 0, death_date: 'چھ مارچ   2025' },
-        { id: 3, name: 'حاجی محمّد زادہ', amount: 1000, deposit_amount: 0, death_date: ' چودہ  اگست  2025' },
-        { id: 4, name: 'شاہ تماز  مرحوم ', amount: 1000, deposit_amount: 0, death_date: 'بارہ دسمبر   2025' },
-        { id: 5, name: 'نیاز محمّد بہن ', amount: 1000, deposit_amount: 0, death_date: 'پانچ اپریل  2026' },
-        { id: 6, name: 'وصی اللہ والدہ ', amount: 1000, deposit_amount: 0, death_date: 'پندرہ جولائی 2026' },
+        { id: 2, name: 'اصغر والدہ', amount: 'وصول', deposit_amount: 1000, death_date: 'چھ مارچ   2025' },
+        { id: 3, name: 'حاجی محمّد زادہ', amount: 'وصول', deposit_amount: 1000, death_date: ' چودہ  اگست  2025' },
+        { id: 4, name: 'شاہ تماز  مرحوم ', amount: 'وصول', deposit_amount: 1000, death_date: 'بارہ دسمبر   2025' },
+        { id: 5, name: 'نیاز محمّد بہن ', amount: 'وصول', deposit_amount: 1000, death_date: 'پانچ اپریل  2026' },
+        { id: 6, name: 'وصی اللہ والدہ ', amount: 'وصول', deposit_amount: 1000, death_date: 'پندرہ جولائی 2026' },
       ]
     }
   },
@@ -200,13 +200,13 @@ export const data: Person[] = [
     urdu: 'سمیع اللہ',
 
     details: {
-      dues: 2000,
+      dues: 0,
       history: [
-        { id: 1, name: 'اصغر والدہ', amount: 1000, deposit_amount: 0, death_date: 'چھ مارچ   2025' },
-        { id: 3, name: 'حاجی محمّد زادہ', amount: 1000, deposit_amount: 0, death_date: ' چودہ  اگست  2025' },
-        { id: 4, name: 'شاہ تماز  مرحوم ', amount: 1000, deposit_amount: 0, death_date: 'بارہ دسمبر   2025' },
-        { id: 5, name: 'نیاز محمّد بہن ', amount: 1000, deposit_amount: 0, death_date: 'پانچ اپریل  2026' },
-        { id: 6, name: 'وصی اللہ والدہ ', amount: 1000, deposit_amount: 0, death_date: 'پندرہ جولائی 2026' },
+        { id: 1, name: 'اصغر والدہ', amount: 'وصول', deposit_amount: 1000, death_date: 'چھ مارچ   2025' },
+        { id: 3, name: 'حاجی محمّد زادہ', amount: 'وصول', deposit_amount: 1000, death_date: ' چودہ  اگست  2025' },
+        { id: 4, name: 'شاہ تماز  مرحوم ', amount: 'وصول', deposit_amount: 1000, death_date: 'بارہ دسمبر   2025' },
+        { id: 5, name: 'نیاز محمّد بہن ', amount: 'وصول', deposit_amount: 1000, death_date: 'پانچ اپریل  2026' },
+        { id: 6, name: 'وصی اللہ والدہ ', amount: 'وصول', deposit_amount: 1000, death_date: 'پندرہ جولائی 2026' },
       ]
     }
   },
